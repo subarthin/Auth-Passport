ad2b7c0c1c3f33d212b2eeea2140c742749de3b1
subarthin raja <subarthinraja@gmail.com>
2023-07-11 19:16:55 +0530

    auto login

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const mongoose=require("mongoose");
 const session=require('express-session');
 const passport=require('passport');
 const passportLocalMongoose=require('passport-local-mongoose');
-const encrypt=require("mongoose-encryption");
+// const encrypt=require("mongoose-encryption");
+const findOrCreate=require('mongoose-findorcreate');
 // const bcrypt=require('bcrypt');
 // const saltrounds=10;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 
 const port=3000;
 const app=express();
@@ -37,7 +40,7 @@ const uSchema=mongoose.Schema({
 
 
 uSchema.plugin(passportLocalMongoose);
-
+uSchema.plugin(findOrCreate)
 
 const User=new mongoose.model("User",uSchema);
 
@@ -46,9 +49,34 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+
+
 app.get("/",function(req,res){
     res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google",{scope:["profile"]})
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 
 app.get("/login",function(req,res){
     res.render("login");
